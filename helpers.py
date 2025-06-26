491276d3e8fde488e71b14d2eaa32cea4fb3be4f
Algorythmsz <qkrtmddnjs1224@naver.com>
2025-06-26 10:42:51 +0200

    load_jsonl

--- helpers.py
+++ helpers.py
@@ -415,3 +415,12 @@ def convert_reference_to_dict(ref_list):
         hard[sid] = sample["hard_labels"]
         soft[sid] = sample["soft_labels"]
     return hard, soft
+
+
+def load_jsonl(file_path):
+    data = []
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            if line.strip():
+                data.append(json.loads(line))
+    return data
